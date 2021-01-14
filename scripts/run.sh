575409035bc92ffe02981118b6966d5ff45ad27a
Attilio Ferraccio <attilio.ferraccio@sourcesense.com>
2021-01-14 10:20:54 +0100

    load different json

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python ./src/run.py ./tests/input_json/json_input_file_2.json 4 "Knife,Potted Plant,Pillow"
+python ./src/run.py ./tests/input_json/json_input_file.json 2 "Knife,Potted Plant"
